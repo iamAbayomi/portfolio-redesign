6cc8f4c40eaa11cb78aae9208650a56cea307ec6
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-03 01:06:54 +0100

    refactor: added header section

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom'
 function Header(){
     return(
         <div>
-            {/* <header> */}
+            <header>
             <Link className="no-underline"  to={'/'}>
                 <p className="home-icon">O.A</p>
             </Link>
             <div id="top-bar" className="nav-id">
                 <a href="https://www.blog.oladiniabayomi.com" target="_blank">BLOG</a>
             </div>
-        {/* </header> */}
+        </header>
         </div>
     )
 }
