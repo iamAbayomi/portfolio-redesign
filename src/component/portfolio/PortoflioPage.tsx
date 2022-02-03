f76ce4918815ab2f3a248d60275776e3da3dfa5a
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-03 01:48:27 +0100

    refactor: changed the section from static to dynamic

--- src/component/portfolio/PortoflioPage.tsx
+++ src/component/portfolio/PortoflioPage.tsx
@@ -1,51 +1,60 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { content } from '../../content'
 import './portfolio.css'
 
 function PortfolioPage (){
-    const params = useParams()
+    const params : any = useParams()
+    let portfolioContent : any
+    const [portfolio, setPortfolio] = useState<any>()
     useEffect(()=>{
-        console.log('This is the params ', params)
+        getPortfolio()
     })
+    
+    function getPortfolio(){
+        portfolioContent = content.find(item => {    
+          return  item.pageUrl == params.pageUrl 
+        })    
+        setPortfolio(portfolioContent)
+    }
+
     return(
         <div className="blog-section">
+            {portfolio !=undefined ?
                 <div className="blog-single-content">
-                    <img className="blog-image" src="/images/hacker-news.jpg" />
-                    <p className="blog-title item-header">Hacker News Clone</p>
+                    <img className="blog-image" src={`/${portfolio.image}`} />
+                    <p className="blog-title item-header">{portfolio.title}</p>
                     <p className="item-header">Overview</p>
                     <p>
-                        Hacker News Clone is a clone of the popular site hacker news.  It is built with react. 
-                        It used the API provided by Hacker News hosted on Firebase.
+                        {portfolio.description}
                     </p>
                     <div className="external-link-section">
                         <div className="websites-section">
                             <p className="small-heading">Website:</p>
-                            <a href="https://iamabayomi-hacker-news-clone.netlify.app/" target="_blank" >https://iamabayomi-hacker-news-clone.netlify.app/</a>
+                            <a href={portfolio.website} target="_blank" >https://iamabayomi-hacker-news-clone.netlify.app/</a>
                         </div>
                         <div className="sourcecode-section">
                             <p className="small-heading">Source Code</p>
-                            <a href="https://github.com/iamAbayomi/react-hacker-news-clone" target="_blank" >https://github.com/iamAbayomi/react-hacker-news-clone</a>
+                            <a href={portfolio.sourceCode} target="_blank" >https://github.com/iamAbayomi/react-hacker-news-clone</a>
                         </div>
                     </div>
                     <div className="section-space-divider"></div>
                     <p className="item-header">Problem Statement</p>
                     <p>
-                        This is the solution to the six fun react projects. The application was meant to give developers practice with lists of 
-                        components and fetching data from the Hacker News API.
+                        {portfolio.problemStatement}
                     </p>
                     <p className="item-header">Solution</p>
                     <p>
-                        I built the application in React and used Axios to fetch data from the Hacker News Firebase API. 
+                    {portfolio.solution}
                     </p>
                     <p className="item-header">Lessons Learned</p>
                     <p>
-                        React only updates what has changed in the real DOM. The process of comparing the old VDOM which is the abstraction layer react adds on the real DOM with the new one is diffing. I made the mistake of using forceUpdate() instead of simply using setState at the end of the API request.
-                        Real DOM updates are slow because they cause an actual re-draw of the UI. React makes this more efficient by updating the smallest amount possible in the real DOM.
-                        It is easy to deploy on Netlify with some command-line commands.
+                    {portfolio.lessonsLearned}
 
                     </p>
                 </div>
-            </div>
+             :<div/>}
+        </div>
     )
 }
 
